82a03934f72c0c5cddebe2e5b0d0a6ed38c27fa6
thanhledh1 <thanhle.vt2@gmail.com>
2023-09-19 10:54:52 +0700

    java1

---
+++ bai6java/mainbt6.js
@@ -0,0 +1,9 @@
+
+        function a(){
+            alert (" xin chào");
+            document.getElementById('ket_qua').innerHTML = ('Xin chào!!');
+            document.write('Xin chào mọi người');
+            console('Hello')
+
+        }
+    
