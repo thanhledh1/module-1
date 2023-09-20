2364988c6035e688bb7a258925fea12ad34c4abc
thanhledh1 <thanhle.vt2@gmail.com>
2023-09-20 10:40:00 +0700

    bai2

---
+++ bai7/main.js
@@ -0,0 +1,6 @@
+var a =  prompt('Nhập vào chiều dài')
+var b = prompt('nhập vào chiều rộng')
+    var dai = parseInt(a)
+    var rong = parseInt(b)
+    var dientich = dai * rong
+    document.write('diện tích là: '+ dientich)
